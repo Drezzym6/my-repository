ec70876b16b311874ee74acda6177f02105e9c8b
andread <andrediya01@gmail.com>
2024-03-25 20:11:36 -0400

    Andre kitten

--- 101-kittens-carousel-static-website-ec2/userdata.sh
+++ 101-kittens-carousel-static-website-ec2/userdata.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 yum update -y
+#install all apache
+yum install httpd -y
+#copy contaent to var/www/html to folder
+cd /var/www/html
+FOLDER="https://raw.githubusercontent.com/awsdevopsteam/101-cfn-static-website-ec2/master/static-web"
+wget ${FOLDER}/index.html
+wget ${FOLDER}/cat0.jpg
+wget ${FOLDER}/cat1.jpg
+wget ${FOLDER}/cat2.jpg
+wget ${FOLDER}/cat3.png

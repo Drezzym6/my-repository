72b81c7c0d38a0264e1fa8d206806df235b35ee8
andread <andrediya01@gmail.com>
2024-03-26 14:35:09 -0400

    kittens updated2

--- 101-kittens-carousel-static-website-ec2/userdata.sh
+++ 101-kittens-carousel-static-website-ec2/userdata.sh
@@ -10,3 +10,7 @@ wget ${FOLDER}/cat0.jpg
 wget ${FOLDER}/cat1.jpg
 wget ${FOLDER}/cat2.jpg
 wget ${FOLDER}/cat3.png
+
+# start and enable Apache service
+systemctl start httpd
+systemctl enable httpd

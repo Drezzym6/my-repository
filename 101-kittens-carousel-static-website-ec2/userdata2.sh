e995865a4875784220c05190ced6adadefbbfc83
andread <andrediya01@gmail.com>
2024-04-29 21:11:34 -0400

    add phonebook files

--- 101-kittens-carousel-static-website-ec2/userdata2.sh
+++ 101-kittens-carousel-static-website-ec2/userdata2.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 yum update -y
-yum install -y
+#install all apache
+yum install httpd -y
+#copy content to /var/www/html folder
 
+cd /var/www/html
+FOLDER="https://raw.githubusercontent.com/Drezzym6/my-repository/main/101-kittens-carousel-static-website-ec2/static-web"
+wget ${FOLDER}/index.html
+wget ${FOLDER}/cat0.jpg
+wget ${FOLDER}/cat1.jpg
+wget ${FOLDER}/cat2.jpg
+
+# start and enable apache service
+systemctl start httpd
+systemctl enable httpd
